845dcfd3f0fe6cfdade9265668d6760b3da57b2c
shaojie12 <3289897943@qq.com>
2019-06-21 16:04:47 +0800

    first commit

---
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,26 @@
+package com.example.demo1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@RestController
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name) {
+		return "hi " + name + ",i am from port:" + port;
+	}
+}
